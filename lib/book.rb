aecbc5d563a04e4231bfc5e12e9fb5fd31d90ec4
Travis Crampton & Jake Glassman <student@epicodus.com>
2015-08-19 10:29:17 -0700

    save and == methods for Book class

--- lib/book.rb
+++ lib/book.rb
@@ -7,15 +7,23 @@ class Book
     @author_id = attributes.fetch(:author_id)
   end
 
-    define_singleton_method(:all) do
+  define_singleton_method(:all) do
     returned_books = DB.exec("SELECT * FROM books;")
     books = []
     returned_books.each() do |book|
       title = book.fetch('title')
       author_id = book.fetch('author_id').to_i()
-      books.push(book.new({:title => title, :author_id => author_id}))
+      books.push(Book.new({:title => title, :author_id => author_id}))
     end
     books
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO books (title, author_id) VALUES ('#{@title}', #{@author_id});")
+  end
+
+  define_method(:==) do |another_book|
+    self.title().==(another_book.title()).&(self.author_id().==(another_book.author_id()))
   end
 
 end
--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -1,8 +1,25 @@
 require('spec_helper')
 
-describe(Book)
+describe(Book) do
   describe('.all') do
     it('starts as an emptoy array for books') do
       expect(Book.all()).to(eq([]))
     end
   end
+
+  describe("#==") do
+    it("is the same book if it has the same title and author id") do
+      book1 = Book.new({:title => "The Hobbit", :author_id => 1})
+      book2 = Book.new({:title => "The Hobbit", :author_id => 1})
+      expect(book1).to(eq(book2))
+    end
+  end
+
+  describe("#save") do
+    it("adds a book to the array of saved books") do
+      test_book = Book.new({:title => "The Hobbit", :author_id => 1})
+      test_book.save()
+      expect(Book.all()).to(eq([test_book]))
+    end
+  end
+end
